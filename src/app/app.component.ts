d9a38f9e64bd93ea59fc5eba9f92e87bf52633a7
= <=>
2017-07-13 17:05:38 +0530

    Angular Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Directives';
+  isVisible=false;
+  firstName:string;
+  title1:string;
+  lastName:string;
+  allStudents:string[] = [];
+  
+  showForm() {
+    this.isVisible=true;
+  }
+
+  hideForm() {
+    this.isVisible=false;
+  }
+
+  addTitle() {
+
+    this.title1 = this.firstName+' '+this.lastName;
+    this.isVisible=true;
+  }
+
+
+  
+  addStudent() {
+
+    this.allStudents.push(this.firstName);
+
+   //alert(this.allStudents.length)
+  }
 }
